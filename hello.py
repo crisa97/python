baff9615bd05720ec563ec6a02fdf1d68c0fc486
crisa97 <crisa97@localhost.localdomain>
2023-01-15 16:50:47 -0500

    add type date

--- hello.py
+++ hello.py
@@ -9,3 +9,10 @@ print('Hello work in python')
     in three 
     line
 """
+# consult type data 
+print(type("i'm date str"))  # type 'str'
+print(type(5))  # type 'int'
+print(type(1.5))  # type 'float'
+print(type(3 + 1j))  # type 'complex'
+print(type(True))  # type 'bool'
+print(type(print("my text string")))  # type 'NoneType'
