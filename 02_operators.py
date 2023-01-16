ae1a9698b0101808684a9f4eadb7eed53bf7d71a
crisa97 <crisa97@localhost.localdomain>
2023-01-15 23:07:44 -0500

    type of operators

---
+++ 02_operators.py
@@ -0,0 +1,57 @@
+# arithmetic operators
+
+# sum
+print(3 + 4)
+# subtraction
+print(3 - 4)
+# multiplication
+print(3 * 4)
+# division
+print(3 / 4)
+# mod
+print(10 % 3)
+# floor division
+print(10 // 3)
+# calculate exponent
+print(2 ** 3)
+# calculate number how operators
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("_____________________________")
+# Concatenate as logical operator
+print("Hi " + "python " + "how are you?")
+print("Hi " + str(5))
+print("Hi " * 5)
+print("Hi " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hi " * int(my_float))
+
+print("_____________________________")
+# comparative operators
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+print(3 > 4 > 2)
+print("_____________________________")
+print("hi" > "python")
+print("hi" < "python")
+# alphabetical ordering ascii
+print("hi" <= "python")
+print("hi" == "python")
+print("hi" != "python")
+# count characters
+print(len("hi") >= len("oo"))
+print("_____________________________")
+# logical operators
+
+print(3 > 4 and "hi" > "python")
+print(3 > 4 or "hi" > "python")
+print(3 < 4 and "hi" < "python")
+print(3 < 4 or "hi" < "python")
+print(3 < 4 or "hi" >  "python")
+print(not(3 > 4) )
